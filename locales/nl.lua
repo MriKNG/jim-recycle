df32a8a80f754176fa5a756bfc8af027ad4b2d7f
MriKNG <ayrton.buyle@hotmail.com>
2026-05-31 13:50:19 +0200

    Added dutch translation

---
+++ locales/nl.lua
@@ -0,0 +1,59 @@
+Loc = Loc or {}
+
+Loc["nl"] = {
+    error = {
+        ["cancel"] = "Geannuleerd",
+        ["no_money"] = "Onvoldoende contant geld",
+        ["wrong_time"] = "Wij zijn momenteel gesloten. We zijn open van ",
+        ["till"] = " tot ",
+        ["off_duty"] = "Je hebt je dienst beëindigd",
+        ["no_mats"] = "Geen recycleerbare materialen om in te wisselen",
+        ["no_bottles"] = "Geen flessen of blikjes om in te wisselen",
+        ["searched"] = "Reeds doorzocht.",
+        ["nothing"] = "Je kon niets vinden.",
+        ["full"] = "Je zakken zitten vol"
+    },
+    success = {
+        ["on_duty"] = "Je hebt je dienst gestart",
+        ["get_trash"] = "Je doorzoekt het afval!",
+        ["get_scrap"] = "Je zoekt naar schroot!",
+        ["get_paid"] = "Betaling ontvangen. Totaal: €",
+    },
+    blip = {
+        ["name"] = "Recyclage",
+    },
+    target = {
+        ["enter"] = "Magazijn betreden",
+        ["exit"] = "Magazijn verlaten",
+        ["duty"] = "Recyclagedienst starten/stoppen",
+        ["trade"] = "Materialen inwisselen",
+        ["sell"] = "Materialen verkopen",
+        ["sell_bottles"] = "Flessen verkopen",
+        ["search"] = "Doorzoeken",
+        ["drop_off"] = "Afleveren",
+        ["search_trash"] = "Afval doorzoeken",
+    },
+    menu = {
+        ["close"] = "Sluiten",
+        ["sell_mats"] = "Materialen verkopen",
+        ["sell_mats_txt"] = "Materialen in bulk verkopen",
+        ["sell_all"] = "Alles verkopen voor €",
+        ["each"] = " per stuk",
+        ["mats_trade"] = "Materialen inwisselen",
+        ["trade_amount"] = "Aantal in bezit: ",
+        ["trade1"] = "1 materiaal inwisselen",
+        ["trade10"] = "10 materialen inwisselen",
+        ["trade100"] = "100 materialen inwisselen",
+        ["trade1000"] = "1000 materialen inwisselen",
+        ["trade"] = "Materialen",
+    },
+    progressbar = {
+        ["search"] = "Doorzoeken",
+    },
+    info = {
+
+        ["sell_all"] = "Alles verkopen voor €",
+		["sell_each"] = "per stuk",
+        ["sell_txt"] = "In bulk verkopen voor contant geld",
+    }
+}
